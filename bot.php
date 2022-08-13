bd7474c3c4f9121d38d27f3fc1b602cb55c93cc4
aliq0098 <110575683+aliq0098@users.noreply.github.com>
2022-08-13 18:42:48 +0430

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,7 +3,7 @@ $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 $chat_id = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
-$message_id = $update["result"]["channel_post"]["message_id"];
+$message_id = $update["channel_post"]["message_id"];
 $token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
 
 function edit_message($message_id){

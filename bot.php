4203133d859714660c09cbe13077d2a3a527fb6f
aliq0098 <110575683+aliq0098@users.noreply.github.com>
2022-08-13 18:34:59 +0430

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,8 +1,8 @@
 <?php
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
-//$chat_id = $update["message"]["chat"]["id"];
-//$message = $update["message"]["text"];
+$chat_id = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
 $message_id = $update["result"]["channel_post"]["message_id"];
 $token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
 
@@ -11,8 +11,17 @@ function edit_message($message_id){
         file_get_contents("https://api.telegram.org/bot5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU/editMessageCaption?chat_id=@mood4it&message_id=$message_id&caption=@mood4it");
     }
 
+function send_message($chat_id,$message_id, $text){
+	  $token = "5395119875:AAG1OSy4z_vpD4-fQySYNneoTfVrUHOTAFw";
+        file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text");
+    }
+
 
 edit_message($message_id);
+
+if($message == "/channel"){send_message($chat_id,$message_id, "@fucoffee");}
+
+
 
 
 ?>

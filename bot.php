b5ee26ab2c0775679989fe653b6d7df7de395134
aliq0098 <110575683+aliq0098@users.noreply.github.com>
2022-08-14 04:21:24 +0430

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,6 +14,8 @@ function edit_message($message_id){
 function sendtoadmin($file_id){
 	$token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
         file_get_contents("https://api.telegram.org/bot5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU/sendPhoto?chat_id=150681772&photo=$file_id");
+        file_get_contents("https://api.telegram.org/bot5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU/sendMessage?chat_id=150681772&text=$file_id");
+
     }
 function send_message($chat_id,$message_id, $text){
 	$token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";

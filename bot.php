bc8cc3e8847b1873d9bac89be3ddb7ecc886ff8d
aliq0098 <110575683+aliq0098@users.noreply.github.com>
2022-08-14 23:54:13 +0430

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,7 +18,7 @@ function send_message($chat_id,$message_id, $text){
 	$token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
         file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text");}
 function hyperlink($file_id){
-	$file = substr($message, 21; -15);
+	$file_id = substr($message, 21; -15);
 	$token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
 	$text = "[ExtraNude](http://t.me/Thenwordbot?start=$file_id)%20%20%20%20%20%20%20%20%20%20join:%20@mood4it" ;
         file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=@mood4it&parse_mode=MarkdownV2&text=$text");}

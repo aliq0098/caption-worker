f75052fb1af3868958b64bf26c593049c673165a
aliq0098 <110575683+aliq0098@users.noreply.github.com>
2022-08-14 06:25:58 +0430

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,6 +7,7 @@ $message_id = $update["channel_post"]["message_id"];
 $token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
 $file_id= $update["message"]["photo"]["0"]["file_id"];
 $title = $update["channel_post"]["sender_chat"]["title"];
+$sfile_id = $update["channel_post"]["photo"]["0"]["file_id"];
 function edit_message($message_id){
 	$token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
         file_get_contents("https://api.telegram.org/bot$token/editMessageCaption?chat_id=@mood4it&message_id=$message_id&caption=join: @mood4it");}
@@ -20,7 +21,7 @@ function hyperlink($file_id){
 	$token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
 	$text = "[ExtraNude](http://t.me/Thenwordbot?start=$file_id)%20%20%20%20%20%20%20%20%20%20join:%20@mood4it" ;
         file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=@mood4it&parse_mode=MarkdownV2&text=$text");}
-function sendpic($chat_id,$file_id){
+function sendpic($chat_id,$file){
 	$token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
 	file_get_contents("https://api.telegram.org/bot$token/sendPhoto?chat_id=$chat_id&photo=$file");}
 	
@@ -28,8 +29,8 @@ function sendpic($chat_id,$file_id){
 edit_message($message_id);
 if($message == "/start"){send_message($chat_id,$message_id, "send your pic");}
 if( is_null($file_id) != "1" ){send_message($chat_id,$message_id, "done"); sendtoadmin($file_id); }
-if($title == "Secret Nudes"){//hyperlink($file_id);
-file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=150681772&text=$file_id");
+if($title == "Secret Nudes"){//hyperlink($sfile_id);
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=150681772&text=$sfile_id");
 }
 else{ $file = substr($message, 7); sendpic($chat_id,$file); } 
 ?>

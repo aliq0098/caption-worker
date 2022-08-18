6106ad807a674c3e626fc03149dc2beb3c486673
aliq0098 <110575683+aliq0098@users.noreply.github.com>
2022-08-18 09:20:26 +0430

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,7 +23,13 @@ function hyperlink($file_id){
         file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=@mood4it&parse_mode=MarkdownV2&text=$text");}
 function sendpic($chat_id,$file){
 	$token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
-	file_get_contents("https://api.telegram.org/bot$token/sendPhoto?chat_id=$chat_id&photo=AgACAgQAAx0CaqEH1AADK".$file."AQADAgADcwADKQQ");}
+	$content = file_get_contents("https://api.telegram.org/bot$token/sendPhoto?chat_id=$chat_id&photo=AgACAgQAAx0CaqEH1AADK".$file."AQADAgADcwADKQQ");
+	$update = json_decode($content, true);
+	$chat_id = $update["message"]["chat"]["id"];
+	$message = $update["message"]["text"];
+	$message_id = $update["channel_post"]["message_id"];
+	sleep(30);
+	file_get_contents("https://api.telegram.org/bot$token/deleteMessage?chat_id=$chat_id&message_id=$message_id);}
 	
 	
 if($title == "in the mood"){edit_message($message_id);}

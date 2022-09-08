12a20b614c6dfd69fd61a6082f58175967a2b047
aliq0098 <110575683+aliq0098@users.noreply.github.com>
2022-09-08 09:29:18 +0430

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,7 +26,9 @@ function sendpic($chat_id,$file){
 	$content = file_get_contents("https://api.telegram.org/bot$token/sendPhoto?chat_id=$chat_id&photo=AgACAgQAAx0CaqEH1AADK".$file."AQADAgADcwADKQQ");
 	$update = json_decode($content, true);
 	$message_id = $update["result"]["message_id"];
-	file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=150681772&text=$message_id");
+	$date = $update["result"]["date"];
+	$time = time();
+	file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=150681772&text=$time");
 	}
 function sendpic1($chat_id,$file){
 	$token = "5489035654:AAFzye9nIUywdX6tCZf5JvQT_7HGrwwZUpU";
